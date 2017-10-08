dd63f3f6747b26d4ecd48cf9cf43ce514a9f4696
Tyler Race <tylerrrace@gmail.com>
2017-10-07 20:38:24 -0700

    Initial commit

---
+++ src/Interface.java
@@ -0,0 +1,10 @@
+
+public class Interface {
+
+	public static void main(String[] args) {
+		//Shape square = new Shape();
+		//square.setColor("Yellow");
+		//System.out.println(square.getColor());
+	}
+
+}
---
+++ src/Shape.java
@@ -0,0 +1,35 @@
+/**
+ * @author Tyler Race
+ * This is a class used a base class for other shapes.
+ */
+public abstract class Shape {
+	private double area;
+	private int xPos, yPos;
+	private String color;
+	
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+	
+	public void moveShape(int newXPos, int newYPos) {
+		xPos = newXPos;
+		yPos = newYPos;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getXPos() {
+		return xPos;
+	}
+	
+	public int getYPos() {
+		return yPos;
+	}
+	
+	abstract public void setArea(double newArea);
+	public double getArea() {
+		return area;
+	}
+}
